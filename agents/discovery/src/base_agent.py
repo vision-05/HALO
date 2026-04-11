d6d857b2d5ae662a625a0a65b2970546004b6b51
vision-05 <t.frankel@outlook.com>
2026-04-12 00:49:37 +0100

    adding encryption

--- agents/discovery/src/base_agent.py
+++ agents/discovery/src/base_agent.py
@@ -8,15 +8,25 @@ class BaseAgent:
         self.name = name
         self.role = role
         self.ctx = zmq.asyncio.Context()
-        self.sock = self.ctx.socket(zmq.ROUTER)
-        self.sock.setsockopt_string(zmq.IDENTITY, self.name)
-        self.port = self.sock.bind_to_random_port("tcp://*")
+        self.router = self.ctx.socket(zmq.ROUTER)
+        self.router.curve_server = True
+        self.router.curve_secretkey = None #our private key
+        self.router.setsockopt_string(zmq.IDENTITY, self.name)
+        self.port = self.router.bind_to_random_port("tcp://*")
         self.peers = {}
+        self.outbound_socks = {}
 
         def verify_peer(peername, peerdata):
             clean_name = peername.split('.')[0]
             print(f"{self.name} discovered {peername} at {peerdata['ip']}:{peerdata['port']}")
             self.peers[clean_name] = peerdata
+            dealer = self.context.socket(zmq.DEALER)
+            self.dealer.curve_server = False
+            self.dealer.curve_publickey = None #our public key
+            self.dealer.curve_secretkey = None #our private key
+            self.dealer.curve_serverkey = None #peer public key
+            dealer.connect(f"tcp://{peerdata['ip']}:{peerdata['port']}")
+            self.outbound_socks[clean_name] = dealer
             self.peer_found_event.set()
 
         self.service = Discovery(name, role, self.port, new_peer_callback=verify_peer)
